40fdd12e5b32f0f570c2f2469ee459e471c45c29
Студент1 <1@DESKTOP-7RO4MU5>
2022-10-29 11:34:10 +0300

    Добавьте файлы проекта.

---
+++ Algaritm/Program.cs
@@ -0,0 +1,19 @@
+﻿using Algaritm;
+
+algaritm algaritm1 = new algaritm();
+Console.WriteLine("Введите число");
+string digit = Console.ReadLine();
+bool parse = int.TryParse(digit, out var count);
+Console.WriteLine("Введите строку");
+string line = Console.ReadLine();
+if(parse == true)
+{
+    algaritm1.Func(count, line);
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Ошибка");
+}
+
+
---
+++ Algaritm/algaritm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algaritm
+{
+    internal class algaritm
+    {
+        private int _count;
+        private string _line;
+
+        public void Func(int count,string line)
+        {
+            _count =  count;
+            _line =  line;
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write(line);
+            }
+        }
+    }
+}
